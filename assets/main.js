6bbaa95510dbcd85eca74f118dde0057f86a8f99
rmquapp <rmquapp@gmail.com>
2016-06-16 23:40:38 -0600

    Scroll to menu sections

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,36 @@
 $(function() {
     $.scrollify({
-            section : ".section",
-            sectionName : "section-name",
-            interstitialSection : "",
-            easing: "easeOutExpo",
-            scrollSpeed: 1100,
-            offset : 0,
-            scrollbars: true,
-            standardScrollElements: "",
-            setHeights: true,
-            before:function() {},
-            after:function() {},
-            afterResize:function() {},
-            afterRender:function() {}
-        });
+        section : ".section",
+        sectionName : "section-name",
+        interstitialSection : "",
+        easing: "easeOutExpo",
+        scrollSpeed: 1100,
+        offset : 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        before:function() {},
+        after:function() {},
+        afterResize:function() {},
+        afterRender:function() {}
     });
+});
+
+$(function () {
+    $("#home_s").click(function() {
+        $.scrollify.move("#home");
+        return false;
+    });
+    $("#about_s").click(function() {
+        $.scrollify.move("#about");
+        return false;
+    });
+    $("#projects_s").click(function() {
+        $.scrollify.move("#projects");
+        return false;
+    });
+    $("#resume_s").click(function() {
+        $.scrollify.move("#resume");
+        return false;
+    });
+});
