de125c7a84386e03cbe41fde2bbefb71fc8848c4
rmquapp <rmquapp@gmail.com>
2016-07-17 01:28:45 -0600

    Somewhat mobile friendly

--- assets/main.js
+++ assets/main.js
@@ -1,38 +1,42 @@
  $(function() {
-    $.scrollify({
-        section: ".section",
-        sectionName: "section-name",
-        interstitialSection: "",
-        easing: "easeOutExpo",
-        scrollSpeed: 1100,
-        offset: 0,
-        scrollbars: true,
-        standardScrollElements: "",
-        setHeights: true,
-        before:function() {},
-        after:function() {},
-        afterResize:function() {},
-        afterRender:function() {}
-    });
+    if (screen.width > 1000) {
+        $.scrollify({
+            section: ".section",
+            sectionName: "section-name",
+            interstitialSection: "",
+            easing: "easeOutExpo",
+            scrollSpeed: 1100,
+            offset: 0,
+            scrollbars: true,
+            standardScrollElements: "",
+            setHeights: true,
+            before:function() {},
+            after:function() {},
+            afterResize:function() {},
+            afterRender:function() {}
+        });
+    }
 });
 
 $(function () {
-    $("#home-s").click(function() {
-        $.scrollify.move("#home");
-        return false;
-    });
-    $("#about-s").click(function() {
-        $.scrollify.move("#about");
-        return false;
-    });
-    $("#projects-s").click(function() {
-        $.scrollify.move("#projects");
-        return false;
-    });
-    $("#resume-s").click(function() {
-        $.scrollify.move("#resume");
-        return false;
-    });
+    if (screen.width > 1000) {
+        $("#home-s").click(function() {
+            $.scrollify.move("#home");
+            return false;
+        });
+        $("#about-s").click(function() {
+            $.scrollify.move("#about");
+            return false;
+        });
+        $("#projects-s").click(function() {
+            $.scrollify.move("#projects");
+            return false;
+        });
+        $("#resume-s").click(function() {
+            $.scrollify.move("#resume");
+            return false;
+        });
+    }
 });
 
 $(document).ready(function() {
