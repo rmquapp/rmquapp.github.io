c8996f684f452ae9051884933cf2f8ecc6fe864c
rmquapp <rmquapp@gmail.com>
2017-02-03 21:17:40 -0700

    Initial commit for SENG 513 Assignment 2

---
+++ assets/code.js
@@ -0,0 +1,16 @@
+//
+// stub
+//
+function getStats(txt) {
+    return {
+        nChars: 123,
+        nWords: 22,
+        nLines: 10,
+        nNonEmptyLines: 22,
+        averageWordLength: 3.3,
+        maxLineLength: 33,
+        palindromes: ["12321", "kayak", "mom"],
+        longestWords: ["xxxxxxxxx", "123444444"],
+        mostFrequentWords: [ "hello(7)", "world(1)" ]
+    };
+}
